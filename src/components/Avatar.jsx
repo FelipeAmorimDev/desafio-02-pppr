f1e443eac6f5abd02c9aba3953afa9dc7de3d0ec
root <root@DESKTOP-2VPR9IB>
2024-03-16 19:21:07 -0300

    ✔ Resolvido: 05 Accordion

--- src/components/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-export function Avatar() {
-  return (
-    <img
-      src="https://avatars.githubusercontent.com/u/115343048?v=4"
-      alt="Avatar do usuário"
-      className="avatar"
-    />
-  )
-}
--- src/components/Skills.jsx
+++
@@ -1,7 +0,0 @@
-export function Skills({ name, emoji, color }) {
-  return (
-    <li className={`skill ${color}`}>
-      {name} {emoji}
-    </li>
-  )
-}
