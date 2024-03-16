b81a2ef0b6e6aa0cca00f88cf6b0b90019a2b79e
root <root@DESKTOP-2VPR9IB>
2024-03-16 18:24:55 -0300

    ✔ Resolvido: 02 Passos

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { Contador } from './Contador'
-
 export function App() {
-  const actualDate = new Date().toLocaleDateString('pt-BR', {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'short',
-    year: 'numeric',
-  })
+  function handleClickBtn({ target }) {
+    console.log(target.textContent)
+  }
 
   return (
-    <div className="container">
-      <Contador type="Intervalo" value="1" />
-      <Contador type="Contagem" value="0" />
-
-      <h2 className="container">Hoje é {actualDate}</h2>
+    <div>
+      <div className="container-close">
+        <button className="close" onClick={handleClickBtn}>
+          Fechar
+        </button>
+      </div>
+      <div className="steps">
+        <div>
+          <div className="numbers">
+            <div className="active">1</div>
+            <div>2</div>
+            <div>3</div>
+          </div>
+          <h1 className="message">Passo 1: Entender o problema do cliente</h1>
+          <div className="buttons">
+            <button onClick={handleClickBtn}>Anterior</button>
+            <button onClick={handleClickBtn}>Próximo</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
