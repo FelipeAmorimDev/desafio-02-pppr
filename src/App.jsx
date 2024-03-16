cf9b2172cd45b3bc44ba0c58ccce7292fa6ffa22
root <root@DESKTOP-2VPR9IB>
2024-03-16 18:37:42 -0300

    ✔ Resolvido: 02 Flashcards

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
 export function App() {
-  function handleClickBtn({ target }) {
-    console.log(target.textContent)
+  const cardsInfo = [
+    {
+      id: 7336,
+      question: 'Do quê aplicações React são feitas?',
+      answer: 'Componentes',
+    },
+    {
+      id: 8832,
+      question: 'Qual é o nome da sintaxe usada para descrever UI no React?',
+      answer: 'JSX',
+    },
+    {
+      id: 3457,
+      question: 'Em qual linguagem o React é baseado?',
+      answer: 'JavaScript',
+    },
+    {
+      id: 9103,
+      question: 'Qual método usar para renderizar uma lista no React?',
+      answer: 'map',
+    },
+    {
+      id: 1297,
+      question: 'Qual empresa criou o React?',
+      answer: 'Meta',
+    },
+    {
+      id: 2002,
+      question: 'Como passar informações para um componente React?',
+      answer: 'Através de props',
+    },
+  ]
+
+  function handleCardClick(e) {
+    console.log(e.target.getAttribute('data-id'))
   }
 
   return (
-    <div>
-      <div className="container-close">
-        <button className="close" onClick={handleClickBtn}>
-          Fechar
-        </button>
-      </div>
-      <div className="steps">
-        <div>
-          <div className="numbers">
-            <div className="active">1</div>
-            <div>2</div>
-            <div>3</div>
+    <div className="flashcards">
+      {cardsInfo.map((card) => {
+        return (
+          <div
+            key={card.id}
+            className="card"
+            data-id={card.id}
+            onClick={handleCardClick}
+          >
+            {card.question}
           </div>
-          <h1 className="message">Passo 1: Entender o problema do cliente</h1>
-          <div className="buttons">
-            <button onClick={handleClickBtn}>Anterior</button>
-            <button onClick={handleClickBtn}>Próximo</button>
-          </div>
-        </div>
-      </div>
+        )
+      })}
     </div>
   )
 }
